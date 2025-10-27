ff28c3c27b78bc10f855271838e25ea8ef98cf3b
Daniel <da16gi@gmail.com>
2025-10-27 23:14:02 +0300

    Some improvements

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Navbar from "./components/Navbar";
 
 export default function App() {
     return (
-        <div>
+        <div className='App'>
             <Navbar />
             <Hero />
             <About />

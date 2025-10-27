9539cbcc6fb0804bc7e200a5c2176d0a02f562fd
Daniel <da16gi@gmail.com>
2025-10-27 16:59:33 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+export default function App() {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline text-blue-600">Hotel Website</h1>
+            <p className="text-xl text-gray-600">Welcome to our hotel website</p>
+            <p className="text-xl text-gray-600">We offer the best hotel services</p>
+            <p className="text-xl text-gray-600">Thank you for visiting our website</p>
+        </div>
+    )
+}

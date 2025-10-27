c24b4727884caece4d2f946341cb9ef98f3502bc
Daniel <da16gi@gmail.com>
2025-10-27 20:16:03 +0300

    Configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 
+
+import About from "./components/About";
+import Hero from "./components/Hero";
+import RoomSection from "./components/RoomSection";
+import Navbar from "./components/Navbar";
+
 export default function App() {
     return (
         <div>
-            <h1 className="text-3xl font-bold underline text-blue-600">Hotel Website</h1>
-            <p className="text-xl text-gray-600">Welcome to our hotel website</p>
-            <p className="text-xl text-gray-600">We offer the best hotel services</p>
-            <p className="text-xl text-gray-600">Thank you for visiting our website</p>
+            <Navbar />
+            <Hero />
+            <About />
+            <RoomSection />
         </div>
     )
 }

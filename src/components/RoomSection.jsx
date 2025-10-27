82641a630bb5b361e91cbde8b56a117b8b26d144
Daniel <da16gi@gmail.com>
2025-10-27 20:25:24 +0300

    Add RoomSection component

---
+++ src/components/RoomSection.jsx
@@ -0,0 +1,8 @@
+
+
+export default function RoomSection() {
+    return (
+        <section id="rooms" className="py-20 md:py-32 bg-gray-50 dark:bg-gray-900">
+            <p className="text-2xl text-white">Rooms</p>
+        </section>
+    )}

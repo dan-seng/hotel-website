d52eb096c8a3870445515843073319565ad9cdcc
Daniel <da16gi@gmail.com>
2025-11-01 21:49:02 +0300

    Add BookingForm and FooterSection components to App

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Hero from "./components/Hero";
 import RoomSection from "./components/RoomSection";
 import Navbar from "./components/Navbar";
 import GallerySection from "./components/GallerySection";
+import BookingForm from "./components/BookingForm";
+import FooterSection from "./components/FooterSection";
 
 export default function App() {
     return (
@@ -14,6 +16,8 @@ export default function App() {
             <About />
             <RoomSection />
             <GallerySection />
+            <BookingForm />
+            <FooterSection />
         </div>
     )
 }
